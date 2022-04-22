6aa10e493bb5a8b68df3a98c8078dacad78d8a90
poms313 <pommine@free.fr>
2022-04-22 11:25:12 +0200

    setup test

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,12 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
+  //given
   render(<App />);
+
+  //when
   const linkElement = screen.getByText(/learn react/i);
+
+  //then
   expect(linkElement).toBeInTheDocument();
 });
